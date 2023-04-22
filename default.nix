2e02b29a82dc029facc58abaa61184ae73a9fa87
Tiago Melo <tiago.blackcode@gmail.com>
2023-04-22 11:43:21 +0100

    move old nixpkgs dependency to the top-level file

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
 {
-  nixpkgs ? import <nixpkgs> {}
+  nixpkgs ? import <nixpkgs> {},
+
+  nixpkgs-21_11 ? import (fetchTarball "https://channels.nixos.org/nixpkgs-21.11-darwin/nixexprs.tar.xz") {
+    localSystem = "x86_64-darwin";
+  }
 }:
 
 with nixpkgs;
 
 {
-  inherit (callPackage ./pkgs/ruby {})
+  inherit (callPackage ./pkgs/ruby { inherit nixpkgs-21_11; })
     ruby_2_1_3
     v8_3_15_11_18;
 
--- pkgs/ruby/default.nix
+++ pkgs/ruby/default.nix
@@ -1,12 +1,8 @@
 {
-  
+  nixpkgs-21_11
 }:
 
 rec {
-  nixpkgs-21_11 = import (fetchTarball "https://channels.nixos.org/nixpkgs-21.11-darwin/nixexprs.tar.xz") {
-    localSystem = "x86_64-darwin";
-  };
-
   ruby_2_1_3 = nixpkgs-21_11.callPackage ./ruby_2_1_3.nix {};
 
   v8_3_15_11_18 = nixpkgs-21_11.callPackage ./v8_3_15_11_18.nix {
